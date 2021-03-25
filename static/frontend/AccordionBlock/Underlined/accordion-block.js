9b2daca92181d6de36fd22bbaaa785e87152c369
Emmanuel <emmanuel@toast.co.nz>
2021-03-26 09:04:38 +1300

    new js added for accordion block

--- static/frontend/AccordionBlock/Underlined/accordion-block.js
+++ static/frontend/AccordionBlock/Underlined/accordion-block.js
@@ -10,8 +10,11 @@ export default class AccordionBlock {
     // Here we can add some custom settings!
     this.settings = {
       class: 'js-accordion-block',
-      duration: 500, //ms
+      duration: 750, //ms
     }
+
+    this.openEvents = 0;
+    this.closeEvents = 0;
 
     // ObjectAssign all the user's options
     for (let key in this.settings) {
@@ -30,8 +33,6 @@ export default class AccordionBlock {
     // Find the item elements. This will be found using this.settings.class followed by --item
     this.elements.items = this.block.querySelectorAll(`.${this.settings.class}--item`) || false;
 
-    console.log(this.elements)
-
     // Ideally we want the elements in an array, not a nodeList
     this.items = [];
 
@@ -42,10 +43,12 @@ export default class AccordionBlock {
       trigger: this.elements.items[i].querySelector(`.${this.settings.class}--trigger`) || false,
       // Find the target element
       target: this.elements.items[i].querySelector(`.${this.settings.class}--target`) || false,
+      open: false,
       // We'll use this for the animations :)
       height: {
         current: 0,
         target: 0,
+        full: 0,
       }
     });
 
@@ -57,79 +60,78 @@ export default class AccordionBlock {
         item.trigger.addEventListener('click', (e) => {
           e.preventDefault();
 
-          // Call the open function
-          (item.item.classList.contains(`${this.settings.class}--open`)) ? this.close(item) : this.open(item);;
-          this.open(item);
+          // Set the item's current height to it's current height ¯\_(ツ)_/¯
+          item.height.current = item.target.offsetHeight;
+
+          // Set the target's height to auto!
+          item.target.style.height = 'auto';
+
+          // Save the target's height
+          item.height.full = item.target.offsetHeight;
+
+          // Set the target's height to auto!
+          item.target.style.height = item.height.current + 'px';
+
+          if (item.open) {
+            // It is no longer open
+            item.open = false;
+            // Toggle some classes for css usage
+            item.item.classList.add(`${this.settings.class}--closed`);
+            item.item.classList.remove(`${this.settings.class}--open`);
+
+            // Set the target height to 0!
+            item.height.target = 0;
+
+            // Now we need to animate this change
+            this.close(item);
+          } else {
+            // It is no longer closed
+            item.open = true;
+            // Toggle some classes for css usage
+            item.item.classList.add(`${this.settings.class}--open`);
+            item.item.classList.remove(`${this.settings.class}--closed`);
+
+             // Set the target height to full!
+            item.height.target = item.height.full;
+
+            // Now we need to animate this change
+            this.open(item);
+          }
         });
       }
     });
   }
 
-  tween(item = false) {
-    console.log('here');
-    if (item) {
+  ease(a,b) {
+    // Some carzy easing maths I dont understand ¯\_(ツ)_/¯
+    return ((1 - (a/b)^2) - 1) * 30 / this.settings.duration * b;
+  }
+
+  open(item = false) {
+    // We need to make sure we have an item, and that it's not already completely open
+    if (item && item.target.offsetHeight < item.height.target) {
       window.requestAnimationFrame(() => {
-        if (item.height.current > item.height.target) {
-        // If the item needs to close - shrink it's current height
-          item.height.current -= Math.floor(item.height.target / (.6 * this.settings.duration / 60));
-        }else if (item.height.current < item.height.target) {
-          // If the item needs to open - grow it's current height
-          item.height.current += Math.floor(item.height.target / (this.settings.duration / 60));
-        }else {
-          // Otherwise end the function!
-          return;
-        }
-
-        // console.log(item);
-
-        // Set the height on the element.
-        item.target.style.height = item.height.current + 'px';
-
-        // Repeat the function
-        this.tween(item);
+        // Add an eased amount to our current height
+        item.height.current += this.ease(item.height.current, item.height.full);
+        // Apply this Math.ceil(height) to our target
+        item.target.style.height = Math.ceil(item.height.current) + 'px';
+        // Continue this function :)
+        this.open(item);
       });
     }
   }
 
-  open(item) {
-    // Toggle some classes for css usage
-    item.item.classList.add(`${this.settings.class}--open`);
-    item.item.classList.remove(`${this.settings.class}--closed`);
-
-    // Set the item's current height to it's current height ¯\_(ツ)_/¯
-    item.height.current = item.target.clientHeight;
-
-    // Start by setting the target's height to auto!
-    item.target.style.height = 'auto';
-
-    // Save the target's height
-    item.height.target = item.target.clientHeight;
-
-    // Set the target's height to back to it's initial state fast!
-    item.target.style.height = item.height.current + 'px';
-
-    // Now we need to animate this change
-    this.tween(item);
+  close(item = false) {
+    // We need to make sure we have an item, and that it's not already completely shut
+    if (item && item.target.offsetHeight > item.height.target) {
+      window.requestAnimationFrame(() => {
+        // Remove an eased amount to our current height
+        item.height.current -= this.ease(item.height.current, item.height.full);
+        // Apply this Math.floor(height) to our target
+        item.target.style.height = Math.floor(item.height.current) + 'px';
+        // Continue this function :)
+        this.close(item);
+      });
+    }
   }
-
-  // close(item) {
-  //   // Toggle some classes for css usage
-  //   item.item.classList.add(`${this.settings.class}--closed`);
-  //   item.item.classList.remove(`${this.settings.class}--open`);
-
-  //   // Set the item's current height to it's current height ¯\_(ツ)_/¯
-  //   item.height.current = item.target.clientHeight;
-
-  //   // Start by setting the target's height to auto!
-  //   item.target.style.height = 0;
-
-  //   // Save the target's height
-  //   item.height.target = item.target.clientHeight;
-
-  //   // Set the target's height to back to it's initial state fast!
-  //   item.target.style.height = item.height.current + 'px';
-
-  //   // Now we need to animate this change
-  //   this.tween(item);
-  // }
 }
