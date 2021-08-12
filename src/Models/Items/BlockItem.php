bc60c4d82caa020771e49753b467dfe43e9ebe72
Leandro Palmieri <p@beetpix.com>
2021-08-13 10:44:32 +1200

    item permissions

--- src/Models/Items/BlockItem.php
+++ src/Models/Items/BlockItem.php
@@ -24,4 +24,42 @@ class BlockItem extends DataObject
         return parent::getCMSFields();
     }
 
+    public function canView($member = null)
+    {
+        if ($this->hasMethod('Parent')) {
+            return $this->Parent()->canView($member);
+        }
+
+        return parent::canView($member);
+    }
+
+    public function canEdit($member = null)
+    {
+        if ($this->hasMethod('Parent')) {
+            return $this->Parent()->canEdit($member);
+        }
+
+        return parent::canEdit($member);
+    }
+
+    public function canDelete($member = null)
+    {
+        if ($this->hasMethod('Parent')) {
+            return $this->Parent()->canDelete($member);
+        }
+
+        return parent::canEdit($member);
+    }
+
+    public function canCreate($member = null, $context = [])
+    {
+        if ($this->hasMethod('Parent')) {
+            return $this->Parent()->canCreate($member, $context);
+        }
+
+        return parent::canCreate($member, $context);
+
+    }
+    
+
 }
